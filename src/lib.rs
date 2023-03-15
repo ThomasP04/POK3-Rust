fd484a519ffcb5ece2de16a33eaf8c654a50c065
ThomasP04 <thomas.pont@centrale-marseille.fr>
2023-03-15 18:51:15 +0100

    modif

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,13 @@ impl<T> Tableau<T> {
         self.taille-=1;
     }
 
+    fn echanger_elements_tableau(&mut self, indice1 : usize, indice2 : usize){
+        if indice1>=self.taille || indice2>=self.taille{
+            panic!("Un des indices est plus grand que la taille du tableau.");
+        }
+        self.elements.swap(indice1, indice2);
+    }
+
 }
 
 fn affiche_element<E>(tableau: &Tableau<E>, i: usize) -> &E {
@@ -67,19 +74,85 @@ fn somme_tableau(tab1 : Tableau<i32>, tab2 : Tableau<i32>) -> Tableau<i32> {
     tab_final
 }
 
+fn tri_tableau(tab: &mut Tableau<i32>) {
+    tri_rapide_tableau(tab, 0, tab.taille);
+}
+
+fn tri_rapide_tableau(tab: &mut Tableau<i32>, deb: usize, fin: usize) {
+    if deb >= fin {
+        return;
+    }
+    let pivot = partitionner(tab, deb, fin - 1);
+    tri_rapide_tableau(tab, deb, pivot);
+    tri_rapide_tableau(tab, pivot + 1, fin);
+}
+
+fn partitionner(tab: &mut Tableau<i32>, deb: usize, fin: usize) -> usize {
+    let valeur_pivot = tab.elements[fin];
+    let mut index_pivot = deb;
+    for i in deb..fin {
+        if tab.elements[i] < valeur_pivot {
+            tab.echanger_elements_tableau(i, index_pivot);
+            index_pivot += 1;
+        }
+    }
+    tab.echanger_elements_tableau(index_pivot, fin);
+    index_pivot
+}
+
+fn fusion_tableaux(tab1 : Tableau<i32>, tab2 : Tableau<i32>)->Tableau<i32>{
+    let mut tableau_final=Tableau::nouveau(tab1.taille+tab2.taille);
+    let mut i =0;
+    let mut j =0;
+
+    while i < tab1.taille && j < tab2.taille {
+        if tab1.elements[i] < tab2.elements[j] {
+            tableau_final.ajouter_element(tab1.elements[i]);
+            i += 1;
+        } else {
+            tableau_final.ajouter_element(tab2.elements[j]);
+            j += 1;
+        }
+    }
+
+    while i < tab1.taille {
+        tableau_final.ajouter_element(tab1.elements[i]);
+        i += 1;
+    }
+
+    while j < tab2.taille {
+        tableau_final.ajouter_element(tab2.elements[j]);
+        j += 1;
+    }
+
+    tableau_final
+
+}
+
+
 fn main() {
     let mut tab: Tableau<i32> = Tableau::nouveau(10);
     println!("{:?}", tab);
     tab.ajouter_element(1);
     println!("{:?}", tab);
 
-    let mut T = Tableau {
-        elements:vec![1,2,3],
+    let mut t = Tableau {
+        elements:vec![3,2,1],
         taille: 3,
-        capacite: 10,
+        capacite: 3,
     };
 
-    println!("{:?}", somme_tableau(tab, T));
+    tri_tableau(&mut t);
+    println!("{:?}", t.elements);
+    
+    let mut t2 = Tableau {
+        elements:vec![1,3,5],
+        taille: 3,
+        capacite: 3,
+    };
+
+    println!("{:?}", fusion_tableaux(t, t2));
+
 }
 
 #[cfg(test)]
@@ -124,7 +197,7 @@ mod tests {
     fn test_lire_element() {
         let mut T = Tableau {
             elements:vec![1,2,3],
-            taille: 3,
+            taille: 10,
             capacite: 10,
         };
         assert_eq!(affiche_element(&T, 2), &3);
